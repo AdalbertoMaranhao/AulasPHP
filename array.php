a0c5d7a73b71a6fb39c85079eba8fbe882733f7f
AdalbertoMaranhao <limadalberto@gmail.com>
2026-03-23 13:19:41 -0300

    Salvando modelo de data

---
+++ array.php
@@ -0,0 +1,21 @@
+<?php
+//Arrays - Conjunto de dados
+//variavel que vai armazenar mais de 1 valor
+
+$variavel = [
+    2,
+    3,
+    134,
+    "abobrinha"
+];
+
+$pessoa = [
+    "Nome" => "Maria",
+    "Idade" => 30,
+    "Altura" => 1.6,
+    "Telefone" => "4002-8922"
+];
+
+//TODO ARRAY NO PHP É INDEXADO OU SEJA POSSUI INDICES OU ENDEREÇOS PARA OS VALORES
+
+echo (date("d/m/Y H:i:s"));
