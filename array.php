e9b2d403f8927cac8b6d70bc9f4f9b423a810b7e
AdalbertoMaranhao <limadalberto@gmail.com>
2026-03-24 11:22:43 -0300

    Foreach e funções do array

--- array.php
+++ array.php
@@ -18,4 +18,37 @@ $pessoa = [
 
 //TODO ARRAY NO PHP É INDEXADO OU SEJA POSSUI INDICES OU ENDEREÇOS PARA OS VALORES
 
-var_dump($pessoa);
+
+//Buscar elemento no ARRAY
+//$variavel[chave];
+for ($i = 0; $i < 4; $i++) {
+    echo ($variavel[$i] . "\n");
+}
+foreach ($variavel as $var) {
+    echo ("$var\n");
+}
+foreach ($pessoa as $chave => $valor) {
+    echo ("$chave: $valor \n");
+}
+
+
+$array = [
+    'a' => 'cachorro',
+    'b' => 'gato',
+    'e' => 'elefante',
+    'f' => 'ganso'
+];
+//Exibe o primeiro elemento do array
+$firstValue = array_first($array);
+var_dump($firstValue);
+//Exibe o ultimo elemento do array
+$lastValue = array_last($array);
+var_dump($lastValue);
+
+//Ordena em ordem crescente
+var_dump(asort($array));
+//Ordena em ordem decrescente
+var_dump(arsort($array));
+
+//Contar a quantidade de itens de um array
+var_dump(count($array));
